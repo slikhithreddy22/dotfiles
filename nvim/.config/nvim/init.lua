d34926390615e70680395d362452a489519f61be
slikhithreddy22 <slikhithreddy22@gmail.com>
2026-05-16 14:37:01 +0530

    nvim setup for primary langs

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -686,16 +686,12 @@ do
   --  See `:help lsp-config` for information about keys and how to configure
   ---@type table<string, vim.lsp.Config>
   local servers = {
-    -- clangd = {},
-    -- gopls = {},
-    -- pyright = {},
-    -- rust_analyzer = {},
-    --
-    -- Some languages (like typescript) have entire language plugins that can be useful:
-    --    https://github.com/pmizio/typescript-tools.nvim
-    --
-    -- But for many setups, the LSP (`ts_ls`) will work just fine
-    -- ts_ls = {},
+    clangd = {}, -- C / C++
+    gopls = {}, -- Go (requires `go` on PATH)
+    pyright = {}, -- Python
+    rust_analyzer = {}, -- Rust (requires `cargo` / rustup)
+    ts_ls = {}, -- JavaScript / TypeScript
+    jdtls = {}, -- Java (requires JDK 17+ on PATH; run `:Mason` to install jdtls)
 
     stylua = {}, -- Used to format Lua code
 
@@ -753,7 +749,12 @@ do
   -- You can press `g?` for help in this menu.
   local ensure_installed = vim.tbl_keys(servers or {})
   vim.list_extend(ensure_installed, {
-    -- You can add other tools here that you want Mason to install
+    'ruff', -- Python format (and lint if you enable kickstart.plugins.lint)
+    'clang-format',
+    'google-java-format',
+    'rustfmt',
+    'prettierd',
+    'prettier',
   })
 
   require('mason-tool-installer').setup { ensure_installed = ensure_installed }
@@ -776,8 +777,15 @@ do
     format_on_save = function(bufnr)
       -- You can specify filetypes to autoformat on save here:
       local enabled_filetypes = {
-        -- lua = true,
-        -- python = true,
+        lua = true,
+        python = true,
+        java = true,
+        c = true,
+        cpp = true,
+        go = true,
+        rust = true,
+        javascript = true,
+        typescript = true,
       }
       if enabled_filetypes[vim.bo[bufnr].filetype] then
         return { timeout_ms = 500 }
@@ -790,12 +798,17 @@ do
     },
     -- You can also specify external formatters in here.
     formatters_by_ft = {
-      -- rust = { 'rustfmt' },
-      -- Conform can also run multiple formatters sequentially
-      -- python = { "isort", "black" },
-      --
-      -- You can use 'stop_after_first' to run the first available formatter from the list
-      -- javascript = { "prettierd", "prettier", stop_after_first = true },
+      lua = { 'stylua' },
+      python = { 'ruff_format' },
+      java = { 'google_java_format' },
+      c = { 'clang_format' },
+      cpp = { 'clang_format' },
+      go = { 'gofmt', 'goimports', stop_after_first = true },
+      rust = { 'rustfmt' },
+      javascript = { 'prettierd', 'prettier', stop_after_first = true },
+      typescript = { 'prettierd', 'prettier', stop_after_first = true },
+      javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+      typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
     },
   }
 
@@ -898,7 +911,26 @@ do
   vim.pack.add { { src = gh 'nvim-treesitter/nvim-treesitter', version = 'main' } }
 
   -- Ensure basic parsers are installed
-  local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' }
+  local parsers = {
+    'bash',
+    'c',
+    'cpp',
+    'diff',
+    'go',
+    'html',
+    'java',
+    'javascript',
+    'lua',
+    'luadoc',
+    'markdown',
+    'markdown_inline',
+    'python',
+    'query',
+    'rust',
+    'typescript',
+    'vim',
+    'vimdoc',
+  }
   require('nvim-treesitter').install(parsers)
 
   ---@param buf integer
